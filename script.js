3a44b57b3b2b54b1624ed05d141eff04e4a1fcd4
Morenike Oyewole <herroyalpianist@gmail.com>
2021-06-11 14:42:52 +0100

    (-_-)

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-//toggler method 1
+// //toggler method 1
 
 const menuIcon = document.querySelector('.ham');
 const navBar = document.querySelector('.nav-links');
 
 menuIcon.addEventListener('click', () => {
-    navBar.classList.toggle('change')
+    navBar.classList.add('show')
 });
 
 
+
 //toggler method 2
+
 // $(document).ready(function(){
 //     $('.ham').click(function(){
-//         $('.nav-links').toggleClass('change')
+//         $('.ham').toggleClass('show')
 //     })
 // })
 
+
+//toggler method 3
+
+// const toggle = () =>{
+//     const burger = document.querySelector('.ham');
+//     const nav = document.querySelector('.nav-links');
+//     burger.addEventListener('click', () =>{
+//         nav.classList.toggle('show');
+//     })
+// }
+// toggle();
