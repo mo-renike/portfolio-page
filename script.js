885810e42c16863c52d676b83424cf8b377a3dcf
Morenike Oyewole <herroyalpianist@gmail.com>
2022-11-30 15:31:04 +0100

    UI adjustments, new projects

--- script.js
+++ script.js
@@ -94,3 +94,19 @@ function sendEmail() {
     Body: message,
   }).then((message) => alert(message));
 }
+
+// import { neonCursor } from "https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js";
+
+// neonCursor({
+//   el: document.getElementById("home"),
+//   shaderPoints: 16,
+//   curvePoints: 80,
+//   curveLerp: 0.5,
+//   radius1: 5,
+//   radius2: 30,
+//   velocityTreshold: 10,
+//   sleepRadiusX: 100,
+//   sleepRadiusY: 100,
+//   sleepTimeCoefX: 0.0025,
+//   sleepTimeCoefY: 0.0025,
+// });
